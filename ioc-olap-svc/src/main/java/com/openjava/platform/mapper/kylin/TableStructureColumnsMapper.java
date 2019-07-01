6668404aa857d5047222a5811ea73c1b4d099fd0
zuoye <zuoy@alltosea.com>
2019-07-01 18:36:12 +0800

    #13-feature-读取hive库底下的表、添加hive库的表到kylin、获取所有模型接口、获取指定项目的模型、修改指定的模型、新增模型、获取Encoding、获取Encoding2、获取表结构信息

---
+++ ioc-olap-svc/src/main/java/com/openjava/platform/mapper/kylin/TableStructureColumnsMapper.java
@@ -0,0 +1,18 @@
+package com.openjava.platform.mapper.kylin;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TableStructureColumnsMapper {
+    @JSONField(name = "id")
+    public String id;
+    @JSONField(name = "name")
+    public String name;
+    @JSONField(name = "datatype")
+    public String datatype;
+    @JSONField(name = "comment")
+    public String comment;
+}
