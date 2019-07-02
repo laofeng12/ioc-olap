3bdeabe5ecbab0704e4ef7bb5a53531f1c9a149b
15158433873@163.com <echofan>
2019-07-03 00:13:01 +0800

    修改样式

--- ioc-olap-ui/src/views/olap/createolap.vue
+++ ioc-olap-ui/src/views/olap/createolap.vue
@@ -4,6 +4,7 @@
     <select-step :class="selectStep === 1?'showBox':'hideBox'"></select-step>
     <create-table-relation :class="selectStep === 2?'showBox':'hideBox'"></create-table-relation>
     <set-filed :class="selectStep === 3?'showBox':'hideBox'"></set-filed>
+    <set-measure :class="selectStep === 4?'showBox':'hideBox'"></set-measure>
   </div>
 </template>
 
@@ -12,8 +13,9 @@ import headBox from '@/components/olapComponent/common/head'
 import selectStep from '@/components/olapComponent/createComponent/selectStep'
 import createTableRelation from '@/components/olapComponent/createComponent/createTableRelation'
 import setFiled from '@/components/olapComponent/createComponent/setFiled'
+import setMeasure from '@/components/olapComponent/createComponent/setMeasure'
 export default {
-  components: { headBox, selectStep, createTableRelation, setFiled },
+  components: { headBox, selectStep, createTableRelation, setFiled, setMeasure },
   data () {
     return {
       selectStep: 1
@@ -21,7 +23,8 @@ export default {
   },
   methods: {
     getStepCountAdd (val) {
-      this.selectStep = val + 1
+      console.log(val)
+      this.selectStep += 1
     },
     getStepCountReduce (val) {
       this.selectStep -= 1
@@ -38,13 +41,26 @@ export default {
     >>>.serchTable{
       height auto
     }
+    >>>.selectStep >>>.el-tabs__content{
+     height calc(100vh - 200px)
+    }
+  }
+  .showBox.selectStep >>>.el-tabs__content{
+    height calc(100vh - 200px)
   }
   .hideBox{
     opacity 0
     height 0
     >>>.serchTable{
       height 0
+      width 0
     }
+    >>>.containers{
+      height 0
+    }
+  }
+  .hideBox.selectStep >>>.el-tabs__content{
+    height 0
   }
 }
 </style>
