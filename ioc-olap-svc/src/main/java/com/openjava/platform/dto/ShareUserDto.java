c9757dff91eac43ef8c89f1d838a5152d9642d99
chenbaozhu <chenbz@alltosea.com>
2019-07-29 16:36:24 +0800

    共享接口读取共享代码修改

--- ioc-olap-svc/src/main/java/com/openjava/platform/dto/ShareUserDto.java
+++ ioc-olap-svc/src/main/java/com/openjava/platform/dto/ShareUserDto.java
@@ -5,11 +5,26 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
+import java.util.Date;
 
 @Getter
 @Setter
 public class ShareUserDto extends OlapShare {
     @ApiModelProperty("分享用户名称")
     private String shareUserName;
+
+    public ShareUserDto(){
+
+    }
+
+    public ShareUserDto(Long id, Long fkId, String fkType, Long shareUserId, Date createTime, Long createId, String createName, String shareUserName){
+        this.setShareId(id);
+        this.setFkId(fkId);
+        this.setFkType(fkType);
+        this.setShareUserId(shareUserId);
+        this.setCreateTime(createTime);
+        this.setCreateId(createId);
+        this.setCreateName(createName);
+        this.setShareUserName(shareUserName);
+    }
 }
