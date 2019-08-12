68b3744bce2e641cc3266ebd74072b54e9a3a26d
zuoye <zuoy@alltosea.com>
2019-08-12 15:36:14 +0800

    #64-feature-定时任务

--- ioc-olap-job-svc/src/main/java/com/ioc/example/job/service/CubeServiceImpl.java
+++ ioc-olap-job-svc/src/main/java/com/ioc/example/job/service/CubeServiceImpl.java
@@ -3,23 +3,16 @@ package com.ioc.example.job.service;
 import com.alibaba.fastjson.JSON;
 import com.openjava.platform.api.kylin.KylinAction;
 import com.openjava.platform.common.HttpClient;
-import com.openjava.platform.domain.OlapTimingrefresh;
-import com.openjava.platform.service.OlapTimingrefreshService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
 import java.text.MessageFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 @Service
 @Transactional
 public class CubeServiceImpl extends KylinAction implements CubeService {
-
-    @Resource
-    private OlapTimingrefreshService olapTimingrefreshService;
 
     @Override
     public void build(String cubeName, Date start, Date end) throws Exception {
@@ -29,16 +22,5 @@ public class CubeServiceImpl extends KylinAction implements CubeService {
         hash.put("startTime", start.getTime());
         hash.put("endTime", end.getTime());
         HttpClient.put(url, JSON.toJSONString(hash), config.authorization, void.class);
-    }
-
-    @Override
-    public List<OlapTimingrefresh> findTiming(int frequencyType) throws Exception {
-        List<OlapTimingrefresh> timingList = olapTimingrefreshService.findTiming(frequencyType);
-        return timingList;
-    }
-
-    @Override
-    public void doSave(OlapTimingrefresh ot) throws Exception {
-        olapTimingrefreshService.doSave(ot);
     }
 }
