c291813b9af880ae646a0ee5221b72f0cef75708
zuoye <zuoy@alltosea.com>
2019-08-21 09:19:45 +0800

    测试类

---
+++ ioc-olap-svc/src/main/java/com/openjava/platform/mapper/kylin/DemoColumn.java
@@ -0,0 +1,18 @@
+package com.openjava.platform.mapper.kylin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class DemoColumn {
+    public String resourceId;
+    public String resourceId1;
+    public Long last_modified;
+    public String version;
+    public String name;
+    public List<DemoColumnData> data;
+}
