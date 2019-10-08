297ee9b1f842d99b1059047de72ad59cd39594e5
zihao <547201949@qq.com>
2019-10-08 16:56:13 +0800

    '删除测试代码'

--- ioc-olap-ui/src/components/analysisComponent/common/DynamicTable.vue
+++ ioc-olap-ui/src/components/analysisComponent/common/DynamicTable.vue
@@ -77,7 +77,6 @@ export default {
       this.$emit('tdClick', item, type)
     },
     handleCurrentChange (val) {
-      debugger
       if (!Number(this.input) && this.input !== '0') {
         return this.$message.error('查询行数只能为数字')
       } else if (Number(this.input) <= 0) {
