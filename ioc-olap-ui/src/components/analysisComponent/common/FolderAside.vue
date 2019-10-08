c308f85ea81a7b25f49b69b9d8a794648b2fc115
caohuayun <caohuayun>
2019-10-08 11:14:16 +0800

    -m修改建模表关系样式

--- ioc-olap-ui/src/components/analysisComponent/common/FolderAside.vue
+++ ioc-olap-ui/src/components/analysisComponent/common/FolderAside.vue
@@ -29,7 +29,7 @@
       </span>
     </el-tree>
     <div  v-if="needNewFolder">
-      <el-button style="width: 100%;padding: 0 16px;color: #0486FE; background: #fff!important;border:border: 1px solid #0486FE;" type="primary" size="small" @click="newFolder">新建文件夹</el-button>
+      <el-button style="width: 100%;padding: 0 16px;color: #0486FE; background: #fff!important;border: 1px solid #0486FE;" type="primary" size="small" @click="newFolder">新建文件夹</el-button>
     </div>
     <el-dialog :title="`${folderForm.isNew ? '新建' : '编辑'}文件夹`" :visible.sync="newVisible" width="30%">
       <el-form :model="folderForm" ref="folderForm" :rules="folderRules">
