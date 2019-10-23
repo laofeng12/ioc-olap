18c0dd9f78678cfd91c33742a99680e24f54af67
zihao <547201949@qq.com>
2019-10-23 11:49:37 +0800

    '用cookies解决项目中的跳转'

--- ioc-olap-ui/src/permission1.js
+++ ioc-olap-ui/src/permission1.js
@@ -1,36 +1,31 @@
 import router from './router'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from './utils/auth'
+import { getToken,getUserInfo } from './utils/auth'
 import store from '@/store'
-import { getSessionStorage } from '@/utils'
 import Cookies from 'js-cookie'
 
 NProgress.configure({ showSpinner: false })// NProgress configuration
 
-const whiteList = ['/login'] // 不重定向白名单
+const whiteList = ['/login', '/home'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start()
-
-  const info = getSessionStorage('userInfo')
-  let userInfo = info ? JSON.parse(info) : {}
-  store.commit('SET_USERINFO', userInfo)
-
   const access_token = getToken()
+  const userInfo = getUserInfo()
+  console.log('44444',userInfo)
+  store.commit('SET_USERINFO', userInfo)
+  // const authorityToken = getToken()
+  // const info = localStorage.getItem('userInfo')
+  // let userInfo = {}
+  // if (info) {
+  //   userInfo = JSON.parse(info)
+  //   store.commit('SET_USERINFO', userInfo)
+  // }
+
   // 如果有token
-  if (access_token) {
-    // 判断是否在创建模型
-    let PATH = to.path.split('/')
-    if (!PATH.includes('createolap')) store.dispatch('resetList')
-    // 判断当前停留在模型哪个页面
-    let pathindex = ['', 'selectStep', 'createTableRelation', 'setFiled', 'setMeasure', 'reloadSet', 'advancedSet', 'completeCreate']
-    store.state.olap.HeadNum = pathindex.indexOf(to.name)
-    // 判断当前是新建还是编辑（根据ModelAllList是否为对象来判断是否为编辑）
-    if (pathindex.includes(to.name)) {
-      to.matched[1].meta.title = to.query.cubeName ? '编辑OLAP模型' : '新建OLAP模型'
-    }
-    if (to.path === '/login') {
-      next({ path: '/home' })
+  if (access_token && userInfo) {
+    if (to.path === '/login' || to.path === '/home') {
+      next({ path: '/index' })
       NProgress.done() // if current page is dashboard will not trigger afterEach hook, so manually handle it
     } else {
       // 重置store 的信息
@@ -61,7 +56,9 @@ router.beforeEach((to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) {
       next()
     } else {
-      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+      // next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+      // next('/home')
+      next('/login')
       NProgress.done()
     }
   }
