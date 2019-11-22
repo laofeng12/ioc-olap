b965aa6495ae994e8e606d4709c6270677b02453
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-22 18:35:00 +0800

    修复同步任务

--- ioc-olap-svc/src/main/java/com/openjava/olap/service/OlapTableSyncServiceImpl.java
+++ ioc-olap-svc/src/main/java/com/openjava/olap/service/OlapTableSyncServiceImpl.java
@@ -3,12 +3,10 @@ package com.openjava.olap.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.extension.exceptions.ApiException;
 import com.openjava.olap.common.DataLakeConfig;
 import com.openjava.olap.common.RestToken;
 import com.openjava.olap.common.kylin.HiveHttpClient;
 import com.openjava.olap.domain.OlapTableSync;
-import com.openjava.olap.mapper.kylin.PreloadTableMapper;
 import com.openjava.olap.mapper.kylin.TableStructureMapper;
 import com.openjava.olap.query.OlapTableSyncParam;
 import com.openjava.olap.repository.OlapTableSyncRepository;
@@ -24,9 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author linchuangang
@@ -74,91 +70,98 @@ public class OlapTableSyncServiceImpl implements OlapTableSyncService,Initializi
 
     @Override
     @Security(session = true)
-    public List<OlapTableSyncVo> available(List<OlapTableSyncParam> params)throws Exception {
+    public Map<String,Object> available(List<OlapTableSyncParam> params)throws Exception {
         //表名的生成：固定前缀+ConcurrentSequence.getInstance().getSequence()。判断是否同步过，查询databaseId,resourceId,userId即可
         Assert.notNull(this.dataLakeConfig.getBatchCreateSyncJobUrl(),"调用批量创建同步任务接口地址不能为空");
+        Map<String,Object> map = new HashMap<>();
+        int success = 1;
+        String msg = "同步成功";
         UserVO user = (UserVO) SsoContext.getUser();
         String token = SsoContext.getToken();
         List<OlapTableSyncVo> results = new ArrayList<>();
-        Iterator<OlapTableSyncParam> iterator = params.iterator();
-        log.info("前端传递的表：{}",JSON.toJSONString(params));
-        while (iterator.hasNext()){//相当于复制一份给results
-            OlapTableSyncParam param = iterator.next();
-            OlapTableSync sync= this.get(param.getDatabaseId(),param.getResourceId(),user.getUserId());
-            //只要是请求成功“批量同步任务接口”并返回成功，则标记为1
-            if (sync != null){
-                if (sync.getSuccess() == null || sync.getSuccess() ==0){//失败的就拿去请求
-                    param.setWriterTableSource(sync.getTableName());
-                    OlapTableSyncVo vo = new OlapTableSyncVo();
-                    vo.setDatabaseId(param.getDatabaseId());
-                    vo.setResourceId(param.getResourceId());
-                    vo.setVirtualTableName(sync.getVirtualTableName());
-                    vo.setWriterTableName(sync.getTableName());
-                    vo.setSyncId(sync.getSyncId());
-                    vo.setIsNew(false);
-                    results.add(vo);
-                }else {
-                    OlapTableSyncVo vo = new OlapTableSyncVo();
-                    vo.setDatabaseId(param.getDatabaseId());
-                    vo.setResourceId(param.getResourceId());
-                    vo.setSuccess(true);
-                    vo.setVirtualTableName(sync.getVirtualTableName());
-                    vo.setWriterTableName(sync.getTableName());
-                    results.add(vo);
-                    iterator.remove();//成功的就不拿去请求了
-                }
-            }else {//从未请求过的也拿去请求
-                //这里设置目标表名的生成规则;虚拟表名+资源id+用户id
-                String tableName = param.getVirtualTableName()+"_"+param.getResourceId()+"_"+user.getUserId();
-                param.setWriterTableSource(tableName);
-                OlapTableSyncVo vo = new OlapTableSyncVo();
-                vo.setDatabaseId(param.getDatabaseId());
-                vo.setResourceId(param.getResourceId());
-                vo.setVirtualTableName(param.getVirtualTableName());
-                vo.setWriterTableName(param.getWriterTableSource());
-                vo.setIsNew(true);
-                results.add(vo);
-            }
-        }
-        log.info("请求创建同步任务的参数:{}",JSON.toJSONString(params));
-        String result = "";
-        if (!params.isEmpty()){
-            result = restToken.postJson(
-                this.dataLakeConfig.getHost()+this.dataLakeConfig.getBatchCreateSyncJobUrl(),
-                params,token);
-        }
-        log.info("请求创建同步任务结果返回:{}",result);
-        JSONObject jsonObject = JSON.parseObject(result);
-        JSONArray array;
-        if (jsonObject!= null && jsonObject.containsKey("data") && (array=jsonObject.getJSONArray("data"))!= null ){
-            array.forEach(s->{
-                JSONObject item = (JSONObject) s;
-                OlapTableSync oo = new OlapTableSync();
-                //循环那些拿去请求的参数
-                results.stream().filter(a->a.getDatabaseId().equals(item.getString("databaseId"))
-                && a.getResourceId().equals(item.getString("resourceId")))
-                .forEach(b->{
-                    oo.setTableName(b.getWriterTableName());//设置真实表名
-                    oo.setDatabaseId(b.getDatabaseId());
-                    oo.setResourceId(b.getResourceId());
-                    b.setSuccess(item.getBoolean("success"));
-                    oo.setIsNew(b.getIsNew());
-                    oo.setSuccess(item.getBoolean("success")?1:0);
-                    oo.setCreateBy(user.getUserId());
-                    //不为空的时候，就是更新，为空则新增
-                    oo.setSyncId(b.getSyncId() == null?ConcurrentSequence.getInstance().getSequence():b.getSyncId());
-                    log.info("保存记录:{}",JSON.toJSONString(oo));
-                    this.save(oo);//保存记录
-                });
-            });
-        }
         try {
+            Iterator<OlapTableSyncParam> iterator = params.iterator();
+            log.info("前端传递的表：{}", JSON.toJSONString(params));
+            while (iterator.hasNext()){//相当于复制一份给results
+                OlapTableSyncParam param = iterator.next();
+                OlapTableSync sync= this.get(param.getDatabaseId(),param.getResourceId(),user.getUserId());
+                //只要是请求成功“批量同步任务接口”并返回成功，则标记为1
+                if (sync != null){
+                    if (sync.getSuccess() == null || sync.getSuccess() ==0){//失败的就拿去请求
+                        param.setWriterTableSource(sync.getTableName());
+                        OlapTableSyncVo vo = new OlapTableSyncVo();
+                        vo.setDatabaseId(param.getDatabaseId());
+                        vo.setResourceId(param.getResourceId());
+                        vo.setVirtualTableName(sync.getVirtualTableName());
+                        vo.setWriterTableName(sync.getTableName());
+                        vo.setSyncId(sync.getSyncId());
+                        vo.setIsNew(false);
+                        results.add(vo);
+                    }else {
+                        OlapTableSyncVo vo = new OlapTableSyncVo();
+                        vo.setDatabaseId(param.getDatabaseId());
+                        vo.setResourceId(param.getResourceId());
+                        vo.setSuccess(true);
+                        vo.setVirtualTableName(sync.getVirtualTableName());
+                        vo.setWriterTableName(sync.getTableName());
+                        results.add(vo);
+                        iterator.remove();//成功的就不拿去请求了
+                    }
+                }else {//从未请求过的也拿去请求
+                    //这里设置目标表名的生成规则;虚拟表名+资源id+用户id
+                    String tableName = param.getVirtualTableName()+"_"+param.getResourceId()+"_"+user.getUserId();
+                    param.setWriterTableSource(tableName);
+                    OlapTableSyncVo vo = new OlapTableSyncVo();
+                    vo.setDatabaseId(param.getDatabaseId());
+                    vo.setResourceId(param.getResourceId());
+                    vo.setVirtualTableName(param.getVirtualTableName());
+                    vo.setWriterTableName(param.getWriterTableSource());
+                    vo.setIsNew(true);
+                    results.add(vo);
+                }
+            }
+            log.info("请求创建同步任务的参数:{}",JSON.toJSONString(params));
+            String result = "";
+            if (!params.isEmpty()){
+                result = restToken.postJson(
+                    this.dataLakeConfig.getHost()+this.dataLakeConfig.getBatchCreateSyncJobUrl(),
+                    params,token);
+            }
+            log.info("请求创建同步任务结果返回:{}",result);
+            JSONObject jsonObject = JSON.parseObject(result);
+            JSONArray array;
+            if (jsonObject!= null && jsonObject.containsKey("data") && (array=jsonObject.getJSONArray("data"))!= null ){
+                array.forEach(s->{
+                    JSONObject item = (JSONObject) s;
+                    OlapTableSync oo = new OlapTableSync();
+                    //循环那些拿去请求的参数
+                    results.stream().filter(a->a.getDatabaseId().equals(item.getString("databaseId"))
+                        && a.getResourceId().equals(item.getString("resourceId")))
+                        .forEach(b->{
+                            oo.setTableName(b.getWriterTableName());//设置真实表名
+                            oo.setDatabaseId(b.getDatabaseId());
+                            oo.setResourceId(b.getResourceId());
+                            b.setSuccess(item.getBoolean("success"));
+                            oo.setIsNew(b.getIsNew());
+                            oo.setSuccess(item.getBoolean("success")?1:0);
+                            oo.setCreateBy(user.getUserId());
+                            //不为空的时候，就是更新，为空则新增
+                            oo.setSyncId(b.getSyncId() == null? ConcurrentSequence.getInstance().getSequence():b.getSyncId());
+                            log.info("保存记录:{}",JSON.toJSONString(oo));
+                            this.save(oo);//保存记录
+                        });
+                });
+            }
             queryHiveTableMeta(results);
         }catch (Exception var1){
             log.error("加载麒麟表结构失败",var1);
-            throw var1;
+            success = 0;
+            msg = "加载麒麟表结构失败";
         }
-        return results;
+        map.put("success",success);
+        map.put("msg",msg);
+        map.put("result",results);
+        return map;
     }
 
     /**
@@ -171,10 +174,7 @@ public class OlapTableSyncServiceImpl implements OlapTableSyncService,Initializi
         String project = ((UserVO)SsoContext.getUser()).getUserId();
         if (list != null && !list.isEmpty()){
             for (OlapTableSyncVo s : list) {
-                PreloadTableMapper result = this.hiveHttpClient.preloadTable(project,DATABASE_NAME,s.getWriterTableName());
-                if (result.getLoaded().isEmpty()){
-                    throw new ApiException("无法加载hive表");
-                }
+                this.hiveHttpClient.preloadTable(project,DATABASE_NAME,s.getWriterTableName());
                 TableStructureMapper meta = this.hiveHttpClient.getTableMeta(project, DATABASE_NAME, s.getWriterTableName());
                 s.setMeta(meta);
             }
