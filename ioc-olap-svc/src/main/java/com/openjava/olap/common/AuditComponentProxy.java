a1ac650abfaaab4e1bb9206b348b5b02196ab27d
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-26 10:35:09 +0800

    去掉多线程提交日志

--- ioc-olap-svc/src/main/java/com/openjava/olap/common/AuditComponentProxy.java
+++ ioc-olap-svc/src/main/java/com/openjava/olap/common/AuditComponentProxy.java
@@ -1,5 +1,6 @@
 package com.openjava.olap.common;
 
+import com.alibaba.fastjson.JSON;
 import com.openjava.audit.auditManagement.component.AuditComponet;
 import com.openjava.audit.auditManagement.vo.AuditLogVO;
 import org.slf4j.Logger;
@@ -7,36 +8,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Component
 public class AuditComponentProxy {
     private AuditComponet auditComponet;
 
     private Logger logger = LoggerFactory.getLogger(AuditComponentProxy.class);
 
-    private ExecutorService executor;
 
     public AuditComponentProxy(AuditComponet auditComponet) {
         Assert.notNull(auditComponet,"审计操作组件不能为空");
         this.auditComponet = auditComponet;
-        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
-        Assert.notNull(executor,"初始化线程池失败");
     }
 
     public void saveAudit(AuditLogParam param) {
         try {
-            executor.execute(() -> {
-                try {
-                    AuditLogVO vo = getLog(param);
-                    auditComponet.saveAuditLog(vo);
-                } catch (Exception e) {
-                    logger.error("保存审计日志失败！", e);
-                }
-            });
-        }catch (Exception var1){
-            logger.error("异步提交执行审计代码保存的任务出错");
+            AuditLogVO vo = getLog(param);
+            auditComponet.saveAuditLog(vo);
+        } catch (Exception e) {
+            logger.error("保存审计日志失败！", e);
         }
     }
 
@@ -53,6 +42,7 @@ public class AuditComponentProxy {
         vo.setDataBeforeOperat(param.getBefore());
         vo.setDataAfterOperat(param.getAfter());
         vo.setType((long)param.getType().getIndex());
+        logger.debug("生成的审计日志实体：{}", JSON.toJSONString(vo));
         return vo;
     }
 
