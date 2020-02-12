aaceb382411812e63cf5a5554a148956bfe91492
AnnieCattice <AnnieCattice@hotmail.com>
2020-02-12 14:27:45 +0800

    文件夹序号系统维护，拖拽更新序号接口

---
+++ ioc-olap-svc/src/main/java/com/openjava/olap/query/OlapFolderSortParam.java
@@ -0,0 +1,14 @@
+package com.openjava.olap.query;
+
+import lombok.Data;
+
+/**
+ * @author linchuangang
+ * @create 2020-02-11 17:35
+ **/
+@Data
+public class OlapFolderSortParam {
+
+    private Long folderId;
+    private Integer sortNum;
+}
